ea4a87b2ecb9f4e613a181fe7d053e694e23909e
ashmont <jacabdz@gmail.com>
2022-11-02 22:23:56 -0400

    pep8

--- api/app.py
+++ api/app.py
@@ -1,103 +1,110 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+
+
 
 app = Flask(__name__)
 CORS(app)
 
 productList = [
     {
-      "key": 1,
-      "productName": "Book",
-      "description": "Leather bound, soft paper",
-      "price": 10,
+        "key": 1,
+        "productName": "Book",
+        "description": "Leather bound, soft paper",
+        "price": 10,
     },
     {
-      "key": 2,
-      "productName": "Phone",
-      "description": "Suspiciously low cost phone",
-      "price": 399,
+        "key": 2,
+        "productName": "Phone",
+        "description": "Suspiciously low cost phone",
+        "price": 399,
     },
     {
-      "key": 3,
-      "productName": "Desk",
-      "description": "Just as good as the real oak",
-      "price": 99,
+        "key": 3,
+        "productName": "Desk",
+        "description": "Just as good as the real oak",
+        "price": 99,
     },
     {
-      "key": 4,
-      "productName": "Weird thing",
-      "description": "Unknown materials used",
-      "price": 1025,
-      "images": {
-        1: "https://i.imgur.com/WuFJV8d.png",
-        2: "https://i.imgur.com/WuFJV8d.png",
-        3: "https://i.imgur.com/WuFJV8d.png",
-        4: "https://i.imgur.com/WuFJV8d.png",
-      }
+        "key": 4,
+        "productName": "Weird thing",
+        "description": "Unknown materials used",
+        "price": 1025,
+        "images": {
+            1: "https://i.imgur.com/WuFJV8d.png",
+            2: "https://i.imgur.com/WuFJV8d.png",
+            3: "https://i.imgur.com/WuFJV8d.png",
+            4: "https://i.imgur.com/WuFJV8d.png",
+        }
     },
 ]
 collections = [
-  {
-    "key": 1,
-    "collectionName": "Self Care",
-    "url": "https://i.imgur.com/WuFJV8d.png",
-  },
-  {
-    "key": 2,
-    "collectionName": "Literature",
-    "url": "https://i.imgur.com/eRSDfrR.png",
-  },
-  {
-    "key": 3,
-    "collectionName": "Games",
-    "url": "https://i.imgur.com/tT7BzCM.png",
-  },
-  {
-    "key": 4,
-    "collectionName": "Furniture",
-    "url": "https://i.imgur.com/eWyjefO.png",
-  },
+    {
+        "key": 1,
+        "collectionName": "Self Care",
+        "url": "https://i.imgur.com/WuFJV8d.png",
+    },
+    {
+        "key": 2,
+        "collectionName": "Literature",
+        "url": "https://i.imgur.com/eRSDfrR.png",
+    },
+    {
+        "key": 3,
+        "collectionName": "Games",
+        "url": "https://i.imgur.com/tT7BzCM.png",
+    },
+    {
+        "key": 4,
+        "collectionName": "Furniture",
+        "url": "https://i.imgur.com/eWyjefO.png",
+    },
 ]
+
 
 @app.route('/hello')
 def hello():
-  return jsonify('hello')
+    return jsonify('hello')
+
 
 @app.route('/collections/', methods=['GET'])
 def getCollections():
-  return jsonify(collections)
+    return jsonify(collections)
+
 
 @app.route('/search/', methods=['POST'])
 def getProduct():
-  searchTerm = request.get_json()
-  itemsFound = []
-  for product in productList:
-      if searchTerm["searchTerm"].lower() in product["productName"].lower():
-          itemsFound.append(product)
-  if len(itemsFound) > 0:
-      print(itemsFound)
-      return jsonify(itemsFound)
-  return('Not found')
+    searchTerm = request.get_json()
+    itemsFound = []
+    for product in productList:
+        if searchTerm["searchTerm"].lower() in product["productName"].lower():
+            itemsFound.append(product)
+    if len(itemsFound) > 0:
+        print(itemsFound)
+        return jsonify(itemsFound)
+    return ('Not found')
 
 # Searchbar endpoint receives a string and returns a list of product names that match
+
+
 @app.route('/searchBar/', methods=['POST'])
 def searchBar():
-  searchTerm = request.get_json()
-  if searchTerm["searchTerm"] == '':
+    searchTerm = request.get_json()
+    if searchTerm["searchTerm"] == '':
+        return jsonify('')
+    itemsFound = []
+    itemTemp = {}
+    for product in productList:
+        if searchTerm["searchTerm"].lower() in product["productName"].lower():
+            itemTemp = {
+                'productName': product["productName"],
+                'key': product['key'],
+            }
+            itemsFound.append(itemTemp)
+    if len(itemsFound) > 0:
+        print(itemsFound)
+        return jsonify(itemsFound)
     return jsonify('')
-  itemsFound = []
-  itemTemp = {}
-  for product in productList:
-      if searchTerm["searchTerm"].lower() in product["productName"].lower():
-          itemTemp = {
-            'productName': product["productName"],
-            'key': product['key'],
-          }
-          itemsFound.append(itemTemp)
-  if len(itemsFound) > 0:
-      print(itemsFound)
-      return jsonify(itemsFound)
-  return jsonify('')
 
 
 if __name__ == '__main__':
